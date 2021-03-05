d2ba9262fb714e245fae888b096d6f45ad2a0024
afei1024 <17610177513@163.com>
2021-03-05 11:19:46 +0800

    Initial commit

---
+++ leetcode/editor/cn/[354]俄罗斯套娃信封问题.py
@@ -0,0 +1,37 @@
+# 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式 (w, h) 出现。当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如
+# 同俄罗斯套娃一样。 
+# 
+#  请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。 
+# 
+#  说明: 
+# 不允许旋转信封。 
+# 
+#  示例: 
+# 
+#  输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+# 输出: 3 
+# 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+#  
+#  Related Topics 二分查找 动态规划 
+#  👍 376 👎 0
+
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        import bisect  # 排序模块
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        print(envelopes)
+        arr = []
+        for _, y in envelopes:
+            loc = bisect.bisect_left(arr, y)
+            arr[loc:loc + 1] = [y]
+            print(arr)
+        print(arr)
+        return len(arr)
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+print(Solution().maxEnvelopes([[5, 4], [6, 4], [6, 7], [2, 3]]))
